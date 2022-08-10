d57ebb5e9f8d65821cb4c473d9e91a92abd9a050
Satyam Mishra <satyam.work.only@gmail.com>
2022-08-10 16:25:02 +0530

    ADDED ADMIN DASHBOARD AND FEW VARIABLES

--- app/app.py
+++ app/app.py
@@ -3,7 +3,7 @@ from app.connectDb import *
 
 name = "Life Overseas"
 app = Flask(name)
-app.secret_key = 'jhsd]e[3984764.573kjs4a1'
+app.secret_key = 'jhsd]e[3984764.573kjs4a1jkrjg876}][}[[.&6s'
 
 Admin_login = {
     "locked":True,
@@ -11,7 +11,7 @@ Admin_login = {
     'adminNo':0
 }
 # Admin Login and panel
-@app.route("/admin/", methods= ["POST","GET"])
+@app.route("/admin", methods= ["POST","GET"])
 def admin_root():
     if request.method == "POST":
         values = request.form
@@ -36,12 +36,28 @@ def admin_root():
 
     return render_template("Admin.html")
 
-@app.route("/admin/dashboard/")
+@app.route("/admin/dashboard")
 def admin_panel():
+    visit_count = realtime_db.child("AppData").child("Website visit count").get().val()
+    total_users = realtime_db.child("AppData").child("Total users").get().val()
+    daily_login = realtime_db.child("AppData").child("Daily logins").get().val()
+    notification_count = realtime_db.child("AppData").child("Notification count").get().val()
+    notifications = {
+
+    }
+
+    admin_data = {
+        "visits": visit_count,
+        "users": total_users,
+        "notification_count": notification_count,
+        "daily_login": daily_login,
+        "all_notification": notifications
+    }
+
     if Admin_login["locked"]==True:
         return redirect(url_for("admin_root"))
     elif Admin_login["locked"]==False:
-        return render_template("Admin-Panel.html")
+        return render_template("Admin-Panel.html",admin_data=admin_data)
     else:
         return redirect(url_for("index"))
 
@@ -52,18 +68,30 @@ def admin_panel():
 # user pages
 @app.route("/")
 def index():
+    visit_count = realtime_db.child("AppData").child("Website visit count").get().val()
+    realtime_db.child("AppData").child("Website visit count").set(visit_count+1)
+
     return render_template("Life-Overseas---Home.html")
 
-@app.route("/about/")
+@app.route("/about")
 def about():
+    visit_count = realtime_db.child("AppData").child("Website visit count").get().val()
+    realtime_db.child("AppData").child("Website visit count").set(visit_count+1)
+
     return render_template("Life-Overseas---About.html")
 
-@app.route("/contact/")
+@app.route("/contact")
 def contact():
+    visit_count = realtime_db.child("AppData").child("Website visit count").get().val()
+    realtime_db.child("AppData").child("Website visit count").set(visit_count+1)
+
     return render_template("Life-Overseas---Contact.html")
 
-@app.route("/faculty/",methods=["GET","POST"])
+@app.route("/faculty",methods=["GET","POST"])
 def faculty():
+    visit_count = realtime_db.child("AppData").child("Website visit count").get().val()
+    realtime_db.child("AppData").child("Website visit count").set(visit_count+1)
+
     questions = dict()
     try:
         questions = dict(realtime_db.child("AppData").child("Questions").get().val())
@@ -81,3 +109,21 @@ def faculty():
             return redirect(url_for('index'))    
         
     return render_template("Life-Overseas---Faculty.html",data=data)
+
+
+
+
+
+@app.route("/user/signin",methods=["GET","POST"])
+def user_login():
+    visit_count = realtime_db.child("AppData").child("Website visit count").get().val()
+    realtime_db.child("AppData").child("Website visit count").set(visit_count+1)
+
+    return render_template("Login page")
+
+@app.route("/user/signup",methods=["GET","POST"])
+def user_login():
+    visit_count = realtime_db.child("AppData").child("Website visit count").get().val()
+    realtime_db.child("AppData").child("Website visit count").set(visit_count+1)
+
+    return render_template("sign up")
